8c7ddc471f20b3bdf4a7f53a8bfae9c0cf99c21c
Lorem Guo <kakuryou@gmail.com>
2022-05-14 04:40:39 +0800

    WIP

---
+++ compontents/MobileMenu/MobileMenu.js
@@ -0,0 +1,13 @@
+import MenuIcon from '@mui/icons-material/Menu';
+import {Grid} from "@material-ui/core";
+
+export default function MobileMenu() {
+    return (
+        <>
+            <Grid container>
+                <MenuIcon/>
+            </Grid>
+
+        </>
+    )
+}
